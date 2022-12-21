36869dc34817db76321c556c97f7adf86f34bbbd
unknown <chahine.benlahcen@gmail.com>
2022-12-21 11:44:33 +0100

    fix

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/components/default_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobile/screens/auth.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,7 +14,19 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Loading'),
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AuthScreen()),
+                  (route) => false);
+            },
+          )
+        ],
       ),
       body: Container(
         alignment: Alignment.center,
