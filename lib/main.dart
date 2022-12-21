78115cfe98a339037a0babbca290cdb30568ad30
unknown <chahine.benlahcen@gmail.com>
2022-12-21 17:24:10 +0100

    clear

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/firebase_options.dart';
 import 'package:mobile/screens/auth.dart';
