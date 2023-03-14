4be46cd8ecb61aa966985a3bbd2922187c5f734f
unknown <chahine.benlahcen@gmail.com>
2023-03-14 15:42:16 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,72 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile/screens/google_sign_in.dart';
 import 'package:mobile/wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(const MaterialApp(
-    home: App(),
+    home: WebViewApp(),
   ));
 }
 
-
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
+  App({Key? key}) : super(key: key);
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   @override
-   Widget build(BuildContext context) => ChangeNotifierProvider(
-      create : (context) => GoogleSignInProviders(),
-      child: const MaterialApp(
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProviders(),
+      child: MaterialApp(
         home: Wrapper(),
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: analytics),
+        ],
       ));
 }
 
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {
+          FirebaseAnalytics.instance.logEvent(
+            name: 'website_visit',
+            parameters: {'url': url},
+          );
+          
+        },
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://google.com'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
